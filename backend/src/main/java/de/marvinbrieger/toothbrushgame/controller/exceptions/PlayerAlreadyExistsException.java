cb6ba1f478106698ac0c924fb71a381dc2b72feb
aiquita <mail@marvinbrieger.de>
2020-03-24 19:40:36 +0100

    Update backend/src/main/java/de/marvinbrieger/toothbrushgame/controller/exceptions/PlayerAlreadyExistsException.java
    Co-Authored-By: Elias Keis <13063245+elKei24@users.noreply.github.com>

--- backend/src/main/java/de/marvinbrieger/toothbrushgame/controller/exceptions/PlayerAlreadyExistsException.java
+++ backend/src/main/java/de/marvinbrieger/toothbrushgame/controller/exceptions/PlayerAlreadyExistsException.java
@@ -5,7 +5,7 @@ import de.marvinbrieger.toothbrushgame.domain.Player;
 public class PlayerAlreadyExistsException extends RuntimeException {
 
     public PlayerAlreadyExistsException(String playerName) {
-        super("A layer with the name " + playerName + " already exists in the game");
+        super("A player with the name " + playerName + " already exists in the game");
     }
 
 }
