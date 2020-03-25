f980ff2f445171753cdcef5def150dd9a9ba3fbb
Marvin Brieger <mail@marvinbrieger.de>
2020-03-25 21:03:40 +0100

    MUR-27: players mock

--- backend/src/main/java/de/marvinbrieger/toothbrushgame/domain/Player.java
+++ backend/src/main/java/de/marvinbrieger/toothbrushgame/domain/Player.java
@@ -1,7 +1,9 @@
 package de.marvinbrieger.toothbrushgame.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,6 +13,8 @@ import javax.persistence.ManyToOne;
 import javax.validation.constraints.Size;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Player {
 
