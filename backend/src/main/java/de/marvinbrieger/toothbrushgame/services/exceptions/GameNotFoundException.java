f3a32c2530644b847a18d93c75ee0b07fbe3d349
Marvin Brieger <mail@marvinbrieger.de>
2020-03-26 22:22:17 +0100

    MUR-24: renameing

--- backend/src/main/java/de/marvinbrieger/toothbrushgame/services/exceptions/GameNotFoundException.java
+++ backend/src/main/java/de/marvinbrieger/toothbrushgame/services/exceptions/GameNotFoundException.java
@@ -2,17 +2,17 @@ package de.marvinbrieger.toothbrushgame.services.exceptions;
 
 import de.marvinbrieger.toothbrushgame.domain.GameStatus;
 
-public class GameNotFoundExeception extends RuntimeException {
+public class GameNotFoundException extends RuntimeException {
 
-    public GameNotFoundExeception(Long id) {
+    public GameNotFoundException(Long id) {
         super("Could not find game " + id);
     }
 
-    public GameNotFoundExeception(String gameCode) {
+    public GameNotFoundException(String gameCode) {
         super("Could not find game " + gameCode);
     }
 
-    public GameNotFoundExeception(Long id, GameStatus destState) {
+    public GameNotFoundException(Long id, GameStatus destState) {
         super("Could not find game " + id + " or game is not in the state " + destState);
     }
 
