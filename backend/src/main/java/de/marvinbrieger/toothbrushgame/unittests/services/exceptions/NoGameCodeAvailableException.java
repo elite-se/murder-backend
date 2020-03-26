f961ddf2d3e6d3854e260ea77207749e6a4c05f1
Elias Keis <13063245+elKei24@users.noreply.github.com>
2020-03-26 12:59:59 +0100

    MUR-35: consistently use default Spring Boot error handler

--- backend/src/main/java/de/marvinbrieger/toothbrushgame/unittests/services/exceptions/NoGameCodeAvailableException.java
+++ backend/src/main/java/de/marvinbrieger/toothbrushgame/unittests/services/exceptions/NoGameCodeAvailableException.java
@@ -1,9 +1,11 @@
 package de.marvinbrieger.toothbrushgame.unittests.services.exceptions;
 
-public class NoGameCodeAvailableException extends RuntimeException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+public class NoGameCodeAvailableException extends RuntimeException {
     public NoGameCodeAvailableException() {
         super("Could not create a new game code");
     }
-
 }
