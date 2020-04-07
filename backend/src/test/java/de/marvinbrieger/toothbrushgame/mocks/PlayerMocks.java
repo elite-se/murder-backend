32b2279fa51934cb37b580b45661c2063053cc30
Elias Keis <13063245+elKei24@users.noreply.github.com>
2020-04-07 15:59:44 +0200

    [MUR-51] fix unit tests

--- backend/src/test/java/de/marvinbrieger/toothbrushgame/mocks/PlayerMocks.java
+++ backend/src/test/java/de/marvinbrieger/toothbrushgame/mocks/PlayerMocks.java
@@ -1,16 +1,22 @@
 package de.marvinbrieger.toothbrushgame.mocks;
 
+import de.marvinbrieger.toothbrushgame.domain.ApplicationUser;
 import de.marvinbrieger.toothbrushgame.domain.Player;
 
+import java.util.HashSet;
+
 public class PlayerMocks {
+    public static final ApplicationUser USER_MARVIN = new ApplicationUser(1L, "marvins device", "marvins password", null, new HashSet<>(),
+            null);
+    public static final ApplicationUser USER_ELIAS = new ApplicationUser(2L, "elias device", "elias password", null, new HashSet<>(), null);
 
-    public static final Player ELIAS = new Player(null, null, "Elias", null, null);
+    public static final Player ELIAS = new Player(null, null, "Elias", USER_ELIAS, null);
 
-    public static final Player STORED_ELIAS = new Player(1L, null, "Elias", null, null);
+    public static final Player STORED_ELIAS = new Player(1L, null, "Elias", USER_ELIAS, null);
 
-    public static final Player MARVIN = new Player(null, null, "Marvin", null, null);
+    public static final Player MARVIN = new Player(null, null, "Marvin", USER_MARVIN, null);
 
-    public static final Player STORED_MARVIN = new Player(2L, null, "Marvin", null, null);
+    public static final Player STORED_MARVIN = new Player(2L, null, "Marvin", USER_MARVIN, null);
 
     public static final Player ALEX = new Player(null, null, "Alex", null, null);
 
